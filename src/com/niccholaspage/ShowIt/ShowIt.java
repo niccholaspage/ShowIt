bca81c97c7c8986f357178b66950c1865fe9ea3f
Nicholas "niccholaspage" Nassar <niccholaspage@gmail.com>
2011-05-17 21:07:39 -0400

    A try at item respawning. Since people keep telling me it's a copy of Showcase, even though it isn't and for other reasons, I'll get the thread deleted

--- src/com/niccholaspage/ShowIt/ShowIt.java
+++ src/com/niccholaspage/ShowIt/ShowIt.java
@@ -1,20 +1,35 @@
 package com.niccholaspage.ShowIt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
 import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShowIt extends JavaPlugin {
+	public List<Item> items = new ArrayList<Item>();
 	private final PlayerListener playerListener = new PlayerListener(){
 		 public void onPlayerPickupItem(PlayerPickupItemEvent event){
 			 Block block = event.getItem().getLocation().getBlock();
 			 if (block.getType() == Material.GLASS){
 				 event.setCancelled(true);
+			 }else {
+				 items.remove(items);
+			 }
+		 }
+		 public void onPlayerDropItem(PlayerDropItemEvent event){
+			 Item item = event.getItemDrop();
+			 Block block = item.getLocation().getBlock();
+			 if (block.getType() == Material.GLASS){
+				 items.add(item);
 			 }
 		 }
 	};
@@ -27,9 +42,20 @@ public class ShowIt extends JavaPlugin {
 		//Create the pluginmanage pm.
 		PluginManager pm = getServer().getPluginManager();
 		//Create PlayerCommand listener
-	    pm.registerEvent(Event.Type.PLAYER_PICKUP_ITEM, this.playerListener, Event.Priority.Normal, this);
+	    pm.registerEvent(Event.Type.PLAYER_PICKUP_ITEM, playerListener, Event.Priority.Normal, this);
+	    pm.registerEvent(Event.Type.PLAYER_DROP_ITEM, playerListener, Event.Priority.Normal, this);
        //Get the infomation from the yml file.
         PluginDescriptionFile pdfFile = this.getDescription();
+        getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable(){
+        	public void run(){
+        		for (Item item : items){
+        			if (item.isDead()){
+        				item = item.getLocation().getWorld().dropItem(item.getLocation(), item.getItemStack().clone());
+        				item.remove();
+        			}
+        		}
+        	}
+        }, 0, 1200);
         //Print that the plugin has been enabled!
         System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
 	}
